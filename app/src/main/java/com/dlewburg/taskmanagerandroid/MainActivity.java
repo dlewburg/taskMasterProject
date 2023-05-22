c15030cedabfa63b279821344cc27758098511ec
Dasha Burgos <dasha.lewis@gmail.com>
2023-05-22 18:41:30 -0400

    skeleton for project and first page done

---
+++ app/src/main/java/com/dlewburg/taskmanagerandroid/MainActivity.java
@@ -0,0 +1,25 @@
+package com.dlewburg.taskmanagerandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button addTaskButton = findViewById(R.id.mainActivityAddTaskButton);
+        addTaskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TextView) findViewById(R.id.))
+            }
+        });
+    }
+
+}
