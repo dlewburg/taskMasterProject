4a82f1ad9109ff5b5a6ef868e02d4ebaed6e2fdd
Dasha Burgos <dasha.lewis@gmail.com>
2023-05-22 21:12:17 -0400

    completed all task page and functionality

--- app/src/main/java/com/dlewburg/taskmanagerandroid/MainActivity.java
+++ app/src/main/java/com/dlewburg/taskmanagerandroid/MainActivity.java
@@ -16,12 +16,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button addTaskButton = findViewById(R.id.mainActivityAddTaskButton);
-        Button allTaskButton = findViewById(R.id.allTaskActivityAllTaskButton);
+        Button allTaskButton = findViewById(R.id.mainActivityAllTasksButton);
 
         addTaskButton.setOnClickListener(v -> {
             Intent goToAddTaskIntent = new Intent(MainActivity.this, AddTaskActivity.class);
             //Start Intent
             startActivity(goToAddTaskIntent);
+        });
+
+        allTaskButton.setOnClickListener(v -> {
+            Intent goToAllTaskIntent = new Intent(MainActivity.this, AllTasksActivity.class);
+            startActivity(goToAllTaskIntent);
         });
 
 
