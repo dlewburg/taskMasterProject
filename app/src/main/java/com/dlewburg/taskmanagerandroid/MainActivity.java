7f01eac8b9deed8a5510dd43431d9e99418361c0
Dasha Burgos <dasha.lewis@gmail.com>
2023-05-22 20:06:02 -0400

    completed add task page layout

--- app/src/main/java/com/dlewburg/taskmanagerandroid/MainActivity.java
+++ app/src/main/java/com/dlewburg/taskmanagerandroid/MainActivity.java
@@ -2,9 +2,11 @@ package com.dlewburg.taskmanagerandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +16,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button addTaskButton = findViewById(R.id.mainActivityAddTaskButton);
-        addTaskButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ((TextView) findViewById(R.id.))
-            }
+//        addTaskButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+////               TextView myTasksTextView = (TextView) findViewById(R.id.mainActivityMyTasksTextView);
+//            }
+//        });
+
+        addTaskButton.setOnClickListener(v -> {
+            Intent goToOrderFormIntent = new Intent(MainActivity.this, AddTaskActivity.class);
+            //Start Intent
+            startActivity(goToOrderFormIntent);
         });
     }
 
